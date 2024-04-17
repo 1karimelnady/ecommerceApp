2570a5ce42397fc6242a2f88abaf191ca4408228
kareem <@1karimelnady>
2024-04-17 15:50:21 +0200

    update

---
+++ lib/app_manager/local_data.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+//App main color
+
+const Color primaryColor = Color(0xFF4b68FF);
+const Color secondaryColor = Color(0xFFFFE24B);
+const Color accentColor = Color(0xFFb0c7ff);
+
+//Text colors
+const Color textPrimaryColor = Color(0xFF333333);
+const Color textSecondaryColor = Color(0xFF6C757D);
+const Color textWhite = Colors.white;
+
+//GRADIENT COLORS
+
+const Gradient gradient = LinearGradient(
+  begin: Alignment(0.0, 0.0),
+  end: Alignment(0.707, -0.707),
+  colors: [
+    Color(0xffff9a9e),
+    Color(0xfffad0c4),
+    Color(0xfffad0c4),
+  ],
+);
+
+//Background colors
+
+const Color light = Color(0xFFF6F6F6);
+const Color dark = Color(0xFF272727);
+const Color primaryBackground = Color(0xFFF3F5FF);
+
+//Background CONTAINERS COLORS
+const Color lightContainer = Color(0xFFF6F6F6);
+Color darkContainer = Colors.white.withOpacity(0.1);
+
+//Button color
+const Color buttonPrimary = Color(0xFF4b68FF);
+const Color buttonSecondary = Color(0xFF6C757D);
+const Color buttonDisabled = Color(0xFFC4C4C4);
+
+//Border color
+const Color borderPrimary = Color(0xFFD9D9D9);
+const Color borderSecondary = Color(0xFFE6E6E6);
+
+//Error and Validations errors
+const Color error = Color(0xFFD32F2F);
+const Color success = Color(0xFF388E3C);
+const Color warning = Color(0xFFF57c00);
+const Color info = Color(0xFF1976D2);
+
+//Neutral shades
+const Color black = Color(0xFF232323);
+const Color darkerGrey = Color(0xFF4F4F4F);
+const Color darkGrey = Color(0xFF939393);
+const Color grey = Color(0xFFE0E0E0);
+const Color softGrey = Color(0xFFF4F4F4);
+const Color lightGrey = Color(0xFFF9F9F9);
+const Color white = Color(0xFFFFFFFF);
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:ecommerce_app/test_screen.dart';
+import 'package:ecommerce_app/utilites/cache_helper.dart';
 import 'package:ecommerce_app/utilites/themes/themes.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
